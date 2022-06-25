b60dcd979c3341c1f266a3dee245ecfb26529902
Cristian_Suarez <cristiandamsuarez@gmail.com>
2022-06-25 18:32:37 -0300

    Pagina Andando

--- Entraga1_CristianSuarez_CristianMarazzita/Entregable1/Entregable1/urls.py
+++ Entraga1_CristianSuarez_CristianMarazzita/Entregable1/Entregable1/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('entregableapp/', include('EntregableApp.urls')),
+    path('entregableapp/', include('Entregable1App.urls')),
 
 ]
